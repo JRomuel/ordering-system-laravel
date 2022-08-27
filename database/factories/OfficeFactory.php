c7f58ca50376a893237601b463cc4817a8d00a1a
JRomuel <johnromuelmendoza@yahoo.com>
2022-08-27 17:04:46 +0800

    updates

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\User;
+use App\Models\Office;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -16,7 +18,14 @@ class OfficeFactory extends Factory
         return [
             'user_id' => User::factory(),
             'title' => $this->faker->sentence,
-            'description' => $this->faker->paragraph
+            'description' => $this->faker->paragraph,
+            'lat' => $this->faker->latitude,
+            'lng' => $this->faker->longitude,
+            'address_line1' => $this->faker->address,
+            'approval_status' => Office::APPROVAL_APPROVED,
+            'hidden' => false,
+            'price_per_day' => $this->faker->numberBetween(1_000, 2_000),
+            'monthly_discount' => 0
         ];
     }
 }
--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Office;
+use App\Models\Reservation;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ReservationFactory extends Factory
@@ -17,7 +20,7 @@ class ReservationFactory extends Factory
             'user_id' => User::factory(),
             'office_id' => Office::factory(),
             'price' => $this->faker->numberBetween(10_000, 20_000),
-            'status' => 1,
+            'status' => Reservation::STATUS_ACTIVE,
             'start_date' => now()->addDay(1)->format('Y-m-d'),
             'end_date' => now()->addDay(5)->format('Y-m-d'),
         ];
